5e98c3934a7dd4cc735724b4641f81ba8fa6aad1
Sam Brothers <samjbrothers@icloud.com>
2024-06-05 12:20:18 -0400

    cover page refactor, added banner at top

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const darkButton = document.getElementById("appearance-button");
+const appearanceButton = document.getElementById("appearance-button");
+const coverBanner = document.getElementById("cover-banner");
 const canvas = document.getElementById("cover-canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,8 +8,11 @@ let lightColor = "#bebebe";
 let darkColor = "#1b1b1b";
 let lightButtonImg = "image_assets/light_button.png";
 let darkButtonImg = "image_assets/dark_button.png";
+let lightCoverImage = "image_assets/light_cover_image.png";
+let darkCoverImage = "image_assets/dark_cover_image.png";
 
-darkButton.onclick = toggleAppearance;
+
+appearanceButton.onclick = toggleAppearance;
 
 startBlinking();
 
@@ -18,13 +22,13 @@ function startBlinking() {
 
 function blinking() {
     if (appearanceMode == "dark") {
-        drawCircle(ctx, 1480, 465, 27, lightColor);
+        drawCircle(ctx, 1180, 275, 25, lightColor);
     }
     else {
-        drawCircle(ctx, 1480, 465, 27, darkColor);
+        drawCircle(ctx, 1180, 275, 25, darkColor);
     }
     window.setTimeout(function(){
-       ctx.clearRect(1450, 400, 1500, 500);
+       ctx.clearRect(1150, 250, 1250, 300);
   },750)
 }
 
@@ -39,18 +43,38 @@ function toggleAppearance() {
 
 function darkMode() {
     document.body.style.backgroundColor = darkColor;
-    darkButton.style.backgroundColor = lightColor;
+
+    coverBanner.style.backgroundColor = lightColor;
+    appearanceButton.style.backgroundColor = lightColor;
+    appearanceButton.style.border = "3px solid #1b1b1b";
     var buttonImage = document.getElementById("button-image");
     buttonImage.src = darkButtonImg;
+
+    var coverImage = document.getElementById("cover-image");
+    coverImage.src = darkCoverImage;
+
+    var textContainer = document.getElementById("text-container")
+    textContainer.style.color = lightColor;
+
     appearanceMode = "dark";
 }
 
 function lightMode() {
     document.body.style.backgroundColor = lightColor;
-    var button = document.getElementById("appearance-button");
-    button.style.backgroundColor = darkColor;
+
+    coverBanner.style.backgroundColor = darkColor;
+    appearanceButton.style.backgroundColor = darkColor;
+    appearanceButton.style.border = "3px solid #bebebe";
+
     var buttonImage = document.getElementById("button-image");
     buttonImage.src = lightButtonImg;
+
+    var coverImage = document.getElementById("cover-image");
+    coverImage.src = lightCoverImage;
+
+    var textContainer = document.getElementById("text-container")
+    textContainer.style.color = darkColor;
+
     appearanceMode = "light";
 }
 
