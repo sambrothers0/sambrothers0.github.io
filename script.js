67986bcb42caaa7726d0460b095d9c4ea5e7a0e8
Sam Brothers <samjbrothers@icloud.com>
2024-06-11 16:24:38 -0400

    visual design adjustments

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ function darkMode() {
     appearanceButton.style.backgroundColor = lightColor;
     appearanceButton.style.border = "3px solid #1b1b1b";
 
-    var buttonImage = document.getElementById("button-image");
-    buttonImage.src = darkButtonImg;
+    var appearanceButtonImage = document.getElementById("appearance-button-image");
+    appearanceButtonImage.src = darkButtonImg;
 
     var coverImage = document.getElementById("cover-image");
     coverImage.src = darkCoverImage;
@@ -69,8 +69,8 @@ function lightMode() {
     appearanceButton.style.backgroundColor = darkColor;
     appearanceButton.style.border = "3px solid #bebebe";
 
-    var buttonImage = document.getElementById("button-image");
-    buttonImage.src = lightButtonImg;
+    var appearanceButtonImage = document.getElementById("appearance-button-image");
+    appearanceButtonImage.src = lightButtonImg;
 
     var coverImage = document.getElementById("cover-image");
     coverImage.src = lightCoverImage;
@@ -115,5 +115,47 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     window.addEventListener('scroll', onScroll);
-    onScroll(); // Check if the element is in view on page load
+    onScroll();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('appearance-button');
+    const image = document.getElementById('appearance-button-image');
+
+    // Flag to track mouse position
+    let mouseX = 0;
+    let mouseY = 0;
+
+    // Update mouse coordinates on mouse move
+    document.addEventListener('mousemove', function (event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    });
+
+    // Function to check if mouse is over the button
+    function isMouseOverElement(element) {
+        const hoveredElement = document.elementFromPoint(mouseX, mouseY);
+        return element.contains(hoveredElement);
+    }
+
+    // Function to force a check of the mouse position
+    function forceCheckMousePosition() {
+        if (isMouseOverElement(button)) {
+            if (appearanceMode == "dark") {
+                image.src = 'image_assets/dark_button_hover.png'; // Change image source
+            }
+            else {
+                image.src = 'image_assets/light_button_hover.png'; // Change image source
+            }
+        } else {
+            if (appearanceMode == "dark") {
+                image.src = 'image_assets/dark_button.png'; // Change image source
+            }
+            else {
+                image.src = 'image_assets/light_button.png'; // Change image source
+            }
+        }
+    }
+    
+    setInterval(forceCheckMousePosition, 1);
 });
