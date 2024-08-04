bb2a08edc32f37a56c600c4599688cc0d1fcaaba
Sam Brothers <samjbrothers@icloud.com>
2024-08-04 17:14:58 -0400

    Updates

--- js/about.9ede4901.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkpersonal_website"]=self["webpackChunkpersonal_website"]||[]).push([[594],{603:function(e,n,s){s.r(n),s.d(n,{default:function(){return b}});var t=s(6768);const u={class:"about"},a=(0,t.Lk)("h1",null,"This is an about page",-1),r=[a];function c(e,n){return(0,t.uX)(),(0,t.CE)("div",u,r)}var i=s(1241);const o={},l=(0,i.A)(o,[["render",c]]);var b=l}}]);
-//# sourceMappingURL=about.9ede4901.js.map
