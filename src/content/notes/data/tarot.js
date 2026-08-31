76862916014ab8c73898ad915f95747d55390bae
Sam Brothers <samjbrothers@icloud.com>
2026-08-31 14:30:56 -0400

    fix tarot note link

--- src/content/notes/data/tarot.js
+++ src/content/notes/data/tarot.js
@@ -4,7 +4,7 @@ export default {
   title: 'Tarot',
   categories: [CATEGORIES.SOFTWARE],
   date: '2026-08',
-  url: 'https://sambrothers.github.io/tarot/docs',
+  url: 'https://sambrothers0.github.io/tarot/docs',
   linkLabel: 'Read the docs',
   body: `See how the Tarot project was created and how it works.`,
   draft: false
