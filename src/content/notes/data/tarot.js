2a4e185d45c61fea21fe0752211a98161de27368
Sam Brothers <samjbrothers@icloud.com>
2026-08-31 14:16:49 -0400

    update tarot project and note

---
+++ src/content/notes/data/tarot.js
@@ -0,0 +1,11 @@
+import { CATEGORIES } from '@/content/notes/categories'
+
+export default {
+  title: 'Tarot',
+  categories: [CATEGORIES.SOFTWARE],
+  date: '2026-08',
+  url: 'https://sambrothers.github.io/tarot/docs',
+  linkLabel: 'Read the docs',
+  body: `See how the Tarot project was created and how it works.`,
+  draft: false
+}
---
+++ src/content/projects/data/tarot.js
@@ -0,0 +1,10 @@
+import { SKILLS } from '@/content/projects/skills'
+
+export default {
+  title: 'Tarot',
+  date: '2026-08',
+  url: 'https://sambrothers0.github.io/tarot',
+  skills: [SKILLS.REACT, SKILLS.TYPESCRIPT, SKILLS.TAILWIND],
+  thumbnail: '/img/projects/tarot.svg',
+  body: `A web application for parsing and visualizing game-world files (MCA format). The project uses React, TypeScript, and Tailwind to let users to upload zipped save files and quickly visualize the locations of a particular block type throughout the world.`
+}
